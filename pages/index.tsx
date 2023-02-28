89dd073b642664335053c123a760b37f28fbc4f1
uhcyzrK <krol.krzysztof.00@gmail.com>
2023-02-28 19:06:33 +0100

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,10 @@ import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+    {/* <h1>Hello Next.js 👋</h1>
     <p>
       <Link href="/about">About</Link>
-    </p>
+    </p> */}
   </Layout>
 )
 
