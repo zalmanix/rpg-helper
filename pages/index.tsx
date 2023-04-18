af9e5a697a5ca8ae48702c6d8f90f26e4894eae3
zalmanix <zalmanix@gmail.com>
2023-04-18 15:31:20 +0200

    supabase initial config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Layout } from "components/Layout/Layout";
+import supabase from "config/supabaseClient";
 
 const IndexPage = () => {
-  return <Layout></Layout>;
+  // const [error, setError] = useState(null);
+  // const [data, setData] = useState(null);
+
+  // console.log(supabase);
+
+  // useEffect(() => {
+  //   const fetch = async () => {
+  //     const { data, error } = await supabase.from("CallOfCthulhu1920").select();
+
+  //     if (error) {
+  //       setError("Error");
+  //       setData(null);
+  //       console.log(error);
+  //     }
+  //     if (data) {
+  //       setData(data);
+  //       console.log(data);
+  //       setError(null);
+  //     }
+  //   };
+
+  //   void fetch();
+  // }, []);
+  async function signInWithGoogle() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+    console.log(data, error);
+  }
+
+  return (
+    <Layout>
+      <div className="bg-red-800 text-white" onClick={void signInWithGoogle}>
+        login test
+      </div>
+    </Layout>
+  );
 };
 
 export default IndexPage;
